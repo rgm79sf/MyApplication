4cdc508ef0c4de751a68a00f9844c52e169fd896
rgm79sf <rxtypescustom29@gmail.com>
2017-07-04 22:26:48 +0900

    media commit

--- app/src/main/java/com/example/rgm79sf/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rgm79sf/myapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.rgm79sf.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonPushMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonPushMe = (Button) findViewById(R.id.btn);
+        buttonPushMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPushMe.setText("push!!");
+                buttonPushMe = (Button) findViewById(R.id.btn);
+            }
+        });
+
+
     }
+
+
 }
